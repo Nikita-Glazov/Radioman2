dcfcae529185c7c4de551c29ef0efb5bc8faa657
Glazov Nikita <niglazov@gmail.com>
2023-06-01 15:53:50 +0300

    New test and function, 100% coverage

--- src/main/java/ru/netology/radioman/Radio.java
+++ src/main/java/ru/netology/radioman/Radio.java
@@ -1,58 +1,85 @@
 package ru.netology.radioman;
 
 public class Radio {
-    private int currentStation;
-    private int volume;
+    private int currentStation  = 0;
+    private int currentVolume = 0;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int minVolume = 0;
+    private int maxVolume = 100;
+
+    public Radio(){
+        this.currentStation = 9;
+    }
+
+    public Radio(int numberOfStations){
+        this.maxStation = numberOfStations - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
+    public int getMaxStation() {
+        return maxStation;
+    }
 
-    public int getVolume() {
-        return volume;
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
     }
 
     public void next() {
-        if (currentStation == 9) {
-            currentStation = 0;
+        if (currentStation == maxStation) {
+            this.currentStation = minStation;
         } else {
-            currentStation++;
+            this.currentStation++;
         }
     }
 
     public void prev() {
-        if (currentStation == 0) {
-            currentStation = 9;
+        if (currentStation == minStation) {
+            this.currentStation = maxStation;
         } else {
-            currentStation--;
+            this.currentStation--;
         }
     }
 
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0 || newCurrentStation > 9) {
+        if (newCurrentStation < minStation || newCurrentStation > maxStation) {
             return;
         }
-        currentStation = newCurrentStation;
+        this.currentStation = newCurrentStation;
     }
 
     public void setVolume(int newVolume) {
-        if ((newVolume < 0) || (newVolume > 10)) {
+        if ((newVolume < minVolume) || (newVolume > maxVolume)) {
             return;
         }
-        volume = newVolume;
+        this.currentVolume = newVolume;
     }
 
     public void increaseTheVolumeByOne() {
-        if (volume == 10) {
+        if (currentVolume == maxVolume) {
             return;
         }
-        volume++;
+        this.currentVolume++;
     }
 
     public void loweringTheVolumeByOne() {
-        if (volume == 0) {
+        if (currentVolume == minVolume) {
             return;
         }
-        volume--;
+        this.currentVolume--;
     }
 }
--- src/test/java/ru/netology/radioman/TestRadio.java
+++ src/test/java/ru/netology/radioman/TestRadio.java
@@ -1,144 +1,97 @@
 package ru.netology.radioman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class TestRadio {
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setNewStationNumber.csv")
-    public void setNewStationNumber(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
+    Radio YoYoFM = new Radio();
+    @Test
+    public void shouldSetNewStation(){
+      YoYoFM.setCurrentStation(0);
+      Assertions.assertEquals(0,YoYoFM.getCurrentStation());
+      YoYoFM.setCurrentStation(10);
+      Assertions.assertEquals(0,YoYoFM.getCurrentStation());
+      YoYoFM.setCurrentStation(-1);
+      Assertions.assertEquals(0,YoYoFM.getCurrentStation());
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setStationUpperOfRange.csv")
-    public void setStationUpperOfRange(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(15);
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
+      YoYoFM.setCurrentStation(1);
+      Assertions.assertEquals(1,YoYoFM.getCurrentStation());
+      YoYoFM.setCurrentStation(9);
+      Assertions.assertEquals(9,YoYoFM.getCurrentStation());
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setStationLowOfRange.csv")
-    public void setStationLowOfRange(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(-2);
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/checkingNextAt9.csv")
-    public void checkingNextAt9(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
-        radio.next();
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/checkingNextNo9.csv")
-    public void checkingNextNo9(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(5);
-        radio.next();
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/checkingPrevAt0.csv")
-    public void checkingPrevAt0(long expected) {
-        Radio radio = new Radio();
-        radio.prev();
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/checkingPrevNo0.csv")
-    public void checkingPrevNo0(long expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(6);
-        radio.prev();
-        long actual = radio.getCurrentStation();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setNewVolume.csv")
-    public void setNewVolume(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(6);
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setVolumeUpperOfRange.csv")
-    public void setVolumeUpperOfRange(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(15);
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
 
     }
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/setVolumeLowOfRange.csv")
-    public void setVolumeLowOfRange(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(-4);
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void shouldPrevStation(){
+        YoYoFM.setCurrentStation(0);
+        YoYoFM.prev();
+        Assertions.assertEquals(9, YoYoFM.getCurrentStation());
+
+        YoYoFM.setCurrentStation(9);
+        YoYoFM.prev();
+        Assertions.assertEquals(8, YoYoFM.getCurrentStation());
     }
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/testMaxVolume.csv")
-    public void testMaxVolume(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(10);
-        radio.increaseTheVolumeByOne();
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void shouldNextStation(){
+        YoYoFM.setCurrentStation(0);
+        YoYoFM.next();
+        Assertions.assertEquals(1, YoYoFM.getCurrentStation());
+
+        YoYoFM.setCurrentStation(9);
+        YoYoFM.next();
+        Assertions.assertEquals(0, YoYoFM.getCurrentStation());
+
     }
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/TestIncreaseVolume.csv")
-    public void TestIncreaseVolume(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(5);
-        radio.increaseTheVolumeByOne();
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void shouldSetNewVolume(){
+        YoYoFM.setVolume(0);
+        Assertions.assertEquals(YoYoFM.getMinVolume(),YoYoFM.getCurrentVolume());
+        YoYoFM.setVolume(100);
+        Assertions.assertEquals(YoYoFM.getMaxVolume(),YoYoFM.getCurrentVolume());
+        YoYoFM.setVolume(-1);
+        Assertions.assertEquals(100,YoYoFM.getCurrentVolume());
+        YoYoFM.setVolume(101);
+        Assertions.assertEquals(100,YoYoFM.getCurrentVolume());
+
+        YoYoFM.setVolume(1);
+        Assertions.assertEquals(1,YoYoFM.getCurrentVolume());
+        YoYoFM.setVolume(99);
+        Assertions.assertEquals(99,YoYoFM.getCurrentVolume());
     }
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/TestMinVolume.csv")
-    public void TestMinVolume(long expected) {
-        Radio radio = new Radio();
-        radio.loweringTheVolumeByOne();
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void nextVolume(){
+        YoYoFM.setVolume(0);
+        YoYoFM.increaseTheVolumeByOne();
+        Assertions.assertEquals(1, YoYoFM.getCurrentVolume());
+
+        YoYoFM.setVolume(100);
+        YoYoFM.increaseTheVolumeByOne();
+        Assertions.assertEquals(YoYoFM.getMaxVolume(), YoYoFM.getCurrentVolume());
     }
 
-    @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/TestLoweringVolume.csv")
-    public void TestLoweringVolume(long expected) {
-        Radio radio = new Radio();
-        radio.setVolume(5);
-        radio.loweringTheVolumeByOne();
-        long actual = radio.getVolume();
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void prevVolume(){
+        YoYoFM.setVolume(0);
+        YoYoFM.loweringTheVolumeByOne();
+        Assertions.assertEquals(YoYoFM.getMinVolume(), YoYoFM.getCurrentVolume());
+
+        YoYoFM.setVolume(100);
+        YoYoFM.loweringTheVolumeByOne();
+        Assertions.assertEquals(99, YoYoFM.getCurrentVolume());
     }
 
+    @Test
+    public void shouldSetNewRange(){
+        Radio YoYoFM1 = new Radio(15);
+        Assertions.assertEquals(14,YoYoFM1.getMaxStation());
+        Assertions.assertEquals(0,YoYoFM1.getMinStation());
 
-
-
-
+        Radio YoYoFM2 = new Radio(1);
+        Assertions.assertEquals(0,YoYoFM2.getMaxStation());
+    }
 }
